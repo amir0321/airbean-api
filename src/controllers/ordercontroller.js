2f3b39201f42cd0d4b2d21d229ea42ae48190f1d
ReggeMeister <mattiaswaern04@gmail.com>
2026-03-29 23:37:02 +0200

    Update ordercontroller.js

--- src/controllers/ordercontroller.js
+++ src/controllers/ordercontroller.js
@@ -2,14 +2,34 @@ const db  = require('..db/database');
 const { v4: uuidv4} = require('uuid');
 
 function createOrder(req, res){
-    const {userId, productId, quantity} = req.body;
+    const {userId, items, campaignId } = req.body;
 
+    // Beräknar totalpris på servern
     let totalPrice = 0;
-    for(const items of items) {
-        const product = db.prepare('SELECT price FROM menu_items WHERE product_id = ? ').length(items.productId);
-        totalPrice += product.price * items.quantity;
+    for(const item of items) {
+        const product = db.prepare('SELECT price FROM menu_items WHERE product_id = ? ').get(item.productId);
+        totalPrice += product.price * item.quantity;
     }
 
 
+
+    // Lägg till  kampanjrabatt
+    if(campaignId){
+
+    }
+
     const orderId = uuidv4();
+
+    const insertOrder = db.prepare('INSERT INTO orders (order_id, user_id, total_price, campaign_id) VALUES (?, ?, ?, ?)');
+    
+    const insertItem = db.prepare('INSERT INTO order_items (order_id, product_id, quantity, unit_price) VALUES (?, ?, ?, ?)');
+
+
+    const createOrderTransaction = db.transaction(() => {
+        insertOrder.RUN(orderId, userId, totalPrice, campaignId || NULL)
+
+        for(const item of items){
+            const product = db.prepare('');     // SKRIV HÄR SENARE
+        }
+    })
 }
