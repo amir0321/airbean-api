61e023ac4c0168aca30a145eabfa07561af47460
ReggeMeister <mattiaswaern04@gmail.com>
2026-03-31 00:58:33 +0200

    Update ordersController.js

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -9,29 +9,29 @@ export async function createOrder(req, res) {
   // Beräkna totalpris på servern
   let totalPrice = 0;
   for (const item of items) {
-    console.log('Letar efter productId:', item.productId);
     const product = await db.get(
       'SELECT price FROM menu WHERE id = ?',
       item.productId
     );
-     console.log('Hittade produkt:', product);
     totalPrice += product.price * item.quantity;
   }
 
-  const orderNr = uuidv4();
-  
+  // Generera unikt ordernummer och beräkna ETA
 
-console.log('totalPrice:', totalPrice);
-console.log('orderNr:', orderNr);
+  const orderNr = uuidv4();
+  const eta = 20;
+
+  // Spara ordern
   await db.run(
-    'INSERT INTO orders (order_nr, user_id, total_price) VALUES (?, ?, ?)',
-    orderNr, resolvedUserId, totalPrice
+    'INSERT INTO orders (order_nr, user_id, total_price, eta) VALUES (?, ?, ?, ?)',
+    orderNr, resolvedUserId, totalPrice, eta
   );
 
+  // Spara orderraderna
   for (const item of items) {
     await db.run(
-      'INSERT INTO order_details (order_nr, product_id) VALUES (?, ?)',
-      orderNr, item.productId
+      'INSERT INTO order_details (order_nr, product_id, quantity) VALUES (?, ?, ?)',
+      orderNr, item.productId, item.quantity
     );
   }
 
@@ -39,6 +39,6 @@ console.log('orderNr:', orderNr);
     message: 'Beställning lagd',
     orderNr,
     totalPrice,
-    
+    eta: `${eta} min`,
   });
 }
