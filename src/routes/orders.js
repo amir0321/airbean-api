8fd1caaa92060e23c21ed6d65d35ab7b3175e174
Yunglarn <yunglarn@gmail.com>
2026-04-03 16:43:18 +0200

    added GET on orders.js for overview of order history

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,6 +4,46 @@ import { validateOrder } from '../middleware/orderValidation.js';
 
 const router = express.Router();
 
+/*
+router.get('/test', (req, res) => {
+    res.send('Testing testing');
+});
+*/
+
 router.post('/', validateOrder, createOrder);
+
+
+//GET översikt orderhistorik
+router.get('/:userId', async (req, res) => {
+
+    const db = req.app.get('db');
+    const { userId } = req.params;
+
+    try {
+        const orders = await db.all(
+            `SELECT order_nr, total_price, eta, order_time 
+             FROM orders 
+             WHERE user_id = ? 
+             ORDER BY order_time DESC`,
+        [userId]
+        );
+
+        if (!orders || orders.length === 0) {
+            return res.status(404).json({message: 'Ingen orderhistorik för användaren'});
+        }
+
+        res.json({userId, 
+            totalOrders: orders.length, 
+            orders});
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Misslyckades att hämta orderhistorik'});
+    }
+});
+
+
+
+
 
 export default router;
